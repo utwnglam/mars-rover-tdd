636e030fc7c97ae801d72bea49e1dac30356b9c1
CHEUNJO2 <cheunjo2@oocl.com>
2021-12-02 16:55:34 +0800

    [MarsRover] [joanne] feat: implement case 1

--- src/main/java/com/afs/tdd/Coordination.java
+++ src/main/java/com/afs/tdd/Coordination.java
@@ -1,6 +1,42 @@
 package com.afs.tdd;
 
 public class Coordination {
-  public Coordination(int x, int y, char direction) {
+  private int x;
+  private int y;
+  private char D;
+
+  public Coordination(int x, int y, char D) {
+    this.x = x;
+    this.y = y;
+    this.D = D;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
+  public char getD() {
+    return D;
+  }
+
+  public void setD(char d) {
+    D = d;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%d %d %c", x, y, D);
   }
 }
--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,16 +1,39 @@
 package com.afs.tdd;
 
 public class MarsRover {
-  private Coordination coordination;
+//  final HashMap DirectionMap: {
+//    NORTH: 'N', EAST: 'E',
+//      SOUTH: 'S', WEST: 'W'}
+  private final Coordination pos;
 
   public MarsRover(Coordination coordination) {
-    this.coordination = coordination;
+    this.pos = coordination;
   }
 
   public void executeAction(char command) {
+    switch (command) {
+      case 'M': {
+        moveForward();
+        break;
+      }
+      case 'L': {}
+      case 'R': {}
+    }
+  }
+
+  private void moveForward() {
+    switch (pos.getD()) {
+      case 'N': {
+        pos.setY(pos.getY()+1);
+        break;
+      }
+      case 'E': {}
+      case 'S': {}
+      case 'W': {}
+    }
   }
 
   public String getStatus() {
-    return "";
+    return pos.toString();
   }
 }
