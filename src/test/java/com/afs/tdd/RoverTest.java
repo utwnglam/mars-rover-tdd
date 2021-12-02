42ce48bcd775b5f58f1ff631ed4feeadba65ff74
CHEUNJO2 <cheunjo2@oocl.com>
2021-12-02 16:30:05 +0800

    [MarsRover] [joanne] feat: other TestCases of 'M'

--- src/test/java/com/afs/tdd/RoverTest.java
+++ src/test/java/com/afs/tdd/RoverTest.java
@@ -14,7 +14,34 @@ class RoverTest {
         assertThat(result).isEqualTo("0 1 N");
     }
 
-//    should_locationY_minus_one_given_direction_S_when_command_M()
-//    should_locationX_add_one_given_direction_E_when_command_M()
-//    should_locationX_minus_one_given_direction_W_when_command_M()
+    @Test
+    public void should_locationY_minus_one_given_direction_S_when_command_M() {
+        MarsRover marsRover = new MarsRover(new Coordination(0, 0, 'W'));
+        char command = 'M';
+
+        marsRover.executeAction(command);
+        String result = marsRover.getStatus();
+        assertThat(result).isEqualTo("-1 0 W");
+    }
+
+    @Test
+    public void should_locationX_add_one_given_direction_E_when_command_M() {
+        MarsRover marsRover = new MarsRover(new Coordination(0, 0, 'E'));
+        char command = 'M';
+
+        marsRover.executeAction(command);
+        String result = marsRover.getStatus();
+        assertThat(result).isEqualTo("1 0 E");
+    }
+
+    @Test
+    public void should_locationX_minus_one_given_direction_W_when_command_M() {
+        MarsRover marsRover = new MarsRover(new Coordination(0, 0, 'S'));
+        char command = 'M';
+
+        marsRover.executeAction(command);
+        String result = marsRover.getStatus();
+        assertThat(result).isEqualTo("0 -1 S");
+    }
+
 }
